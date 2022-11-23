00eaa137e75dcab40874239adbcb76f4a0447f73
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2022-11-23 21:30:00 +0500

    Initial Commit

---
+++ PersonalQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Дмитрий Поляков on 23.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
