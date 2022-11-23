42aff9975b922ce6b9ac00b10a724f34cc2a5f94
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2022-11-23 23:51:58 +0500

    The getQuestions() function was created, which returns the model data

--- PersonalQuiz/Model/Question.swift
+++ PersonalQuiz/Model/Question.swift
@@ -9,6 +9,41 @@ struct Question {
     let title: String
     let responseType: ResponseType
     let answers: [Answer]
+    
+    func getQuestions() -> [Question] {
+        [
+            Question(
+                title: "Какую пищу вы предпочитаете?",
+                responseType: .single,
+                answers: [
+                    Answer(title: "Стейк", animal: .dog),
+                    Answer(title: "Рыба", animal: .cat),
+                    Answer(title: "Морковь", animal: .rabbit),
+                    Answer(title: "Кукуруза", animal: .turtle)
+                ]
+            ),
+            Question(
+                title: "Что вам нравится больше?",
+                responseType: .multiple,
+                answers: [
+                    Answer(title: "Плавать", animal: .dog),
+                    Answer(title: "Спать", animal: .cat),
+                    Answer(title: "Обнимать", animal: .rabbit),
+                    Answer(title: "Есть", animal: .turtle)
+                ]
+            ),
+            Question(
+                title: "Любите ли вы поездки на машине?",
+                responseType: .single,
+                answers: [
+                    Answer(title: "Ненавижу", animal: .cat),
+                    Answer(title: "Нервничаю", animal: .rabbit),
+                    Answer(title: "Не замечаю", animal: .turtle),
+                    Answer(title: "Обожаю", animal: .dog)
+                ]
+            )
+        ]
+    }
 }
 
 enum ResponseType {
